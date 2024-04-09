112cf70ed8367769552c78243f9a6859d522b0ae
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-04-09 12:27:33 +0200

    turn comments from it to en and  adjust some vars name

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import functions
 import sys
 
 # ESECUZIONE DEL PROGRAMMA
+# TODO introduce a cl to get the password from a block note path, then
 def main(argomenti):
     for password in argomenti:
         count = functions.conta_trapelate(functions.pwned_API_check(password))
         if count:
             print(f'{password} è stata hackerata {count} volte')
+        # TODO introduce a suggestion to create a password
         else:
             print(f'{password} è sicura')
     return 'finito'
-
+# TODO blank the note and then overwrite in the same location to remove definitely the password existence from the local
 if __name__ == '__main__' :
     sys.exit(main(sys.argv[1:]))
     
