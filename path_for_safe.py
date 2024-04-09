c1033fef74f0e809eaa15f7d1c768e094731001c
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-04-09 12:45:27 +0200

    creation of the file for the blocknote of passwords

---
+++ path_for_safe.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+def check_blank_note(path : str) -> None:
+    '''
+    check if the note is blank
+    '''
+    pass
+
+def overwrite_blanck_note(path : str) -> None:
+    '''
+    overwrite blank the note
+    '''
+    pass
+
+
