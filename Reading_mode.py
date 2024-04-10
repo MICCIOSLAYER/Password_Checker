e37dd8c32334d3fb664d1ff2826d87f31eae48ed
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-04-10 12:32:37 +0200

    added description function in REading mode

--- Reading_mode.py
+++ Reading_mode.py
@@ -19,3 +19,12 @@ def read_from_file(path : str) -> list: # if is_path is True, the path is a file
 def read_from_list(passwords : list) -> list:
     passwords_list = passwords[:]
     return passwords_list
+
+def Description(None) -> str:
+    print('''this is a program to check the security of your passwords: \n
+          please compose the command as follows: python3 main.py <PROTOCOL> <PATH>, where: \n
+          - <PROTOCOL> is the protocol to use (sha1 or sha256) \n
+          - <PATH> is the path to the file containing the passwords to check \n
+          - if you want to check a list of passwords, please insert them as arguments in place of <PATH>,
+          however this option is strongly discourage since you lose all your control
+          in where the password name is saved in current local disk \n''')
