77940a5a29fc4275c0817fa88d7e5427a5fd6be6
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-04-10 12:32:10 +0200

    added a flag for the protocol and adjust index

--- main.py
+++ main.py
@@ -7,23 +7,25 @@ import sys
 import path_for_safe
 # ESECUZIONE DEL PROGRAMMA
 
-def main(argomenti):
+def main(list_of_interest : list) -> str: # from a list of object get the password to check and the protocol to use
+# FIXME add a check for len(list_of_interest) > 1 otherwise the program will stopped? or can we make a while try?
     file_path = False   # initialize to use as a flag
-    
-    if os.path.exists(argomenti[0]): #in case the first term is a file:
+    sha256 = (str(list_of_interest[0]).lower() == 'sha256') #to use the selection of sha256 or sha1 in pwned_API_check
+
+    if os.path.exists(list_of_interest[1]): #in case the second term is a file:
         try: # control if it is empty
-            if path_for_safe.note_is_empty(argomenti[0]):
+            if path_for_safe.note_is_empty(list_of_interest[1]):
                 sys.exit('please insert some passwords to check in the file')
-            file_path = argomenti[0] 
+            file_path = list_of_interest[1] 
             passwords_list = Reading_mode.read_from_file(file_path)
         except FileNotFoundError:    # to not crush in case file is not found
-            sys.exit(f'file {argomenti[0]} not found')
+            sys.exit(f'file {list_of_interest[1]} not found')
 
     else :
-        passwords_list = argomenti  # if not a path assume there's a list of passwords
+        passwords_list = list_of_interest  # if not a path assume there's a list of passwords
 
     for password in passwords_list:
-        count = API_functions.conta_trapelate(API_functions.pwned_API_check(password))
+        count = API_functions.conta_trapelate(API_functions.pwned_API_check(password, sha256))
         if count:
             print(f'\'{password}\' has been hacked {count} times')
         # TODO introduce a suggestion to create a password
@@ -36,6 +38,7 @@ def main(argomenti):
     return 'something else to check?'
 
 if __name__ == '__main__' :
+    
     sys.exit(main(sys.argv[1:]))
     
     
