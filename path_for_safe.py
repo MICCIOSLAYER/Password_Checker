49139ef19f8afd2d7d251bfdfd3959d1d9dfe1da
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-04-10 16:33:37 +0200

    introducing encoding specification, removed imported modules non used

--- path_for_safe.py
+++ path_for_safe.py
@@ -1,25 +1,19 @@
-import os
-import sys
-
 def note_is_empty(path : str) -> bool:
     '''
     check if the note is blank
     '''
-    with open(path, 'r') as f:
+    with open(path, 'r', encoding='utf-8') as f: # TODO test the encoding
         if f.read() == '':
             print('the note is blank, no passwords are stored here')
             return True
         else:
-            return False
-    
+            return False 
 
 def overwrite_blanck_note(path : str) -> None:
     '''
     overwrite blank the note
     '''
-    with open(path, 'w') as f:
+    with open(path, 'w', encoding='utf-8') as f:
         f.write('')
     print('the note is now blank, your passwords are safe')
     return None
-
-
