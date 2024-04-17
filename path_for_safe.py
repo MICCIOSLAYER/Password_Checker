0b672ce33b5d4b7d7f267373c3363012866ce66b
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-04-17 12:28:08 +0200

    update the path_for_safe with pathlib.Path library and assertion and debugging in functions_ to be tested

--- path_for_safe.py
+++ path_for_safe.py
@@ -7,32 +7,57 @@ def txt_to_list(path : str ) -> list:
     Parameters:
     path : str - the absolute-/path of the note to check
     '''
-    #txt_file = pathlib.Path(path) HACK?
-    #while(txt_file.exists()):
-    while True:
-        try:
-            txt_file = pathlib.Path(path)
-            with open(txt_file, 'r', encoding='utf-8') as f:
-                assert f.read() != '', 'the note is blank, no passwords are stored here'
-                passwords_list = f.read().splitlines()
 
-                return passwords_list
-        
-        except FileNotFoundError:
-            print(f'file {path} not found')
-            break
-        except 
+    #txt_file = pathlib.Path(path) HACK?
+    #while('txt' in path):
+    try:
+        txt_file = pathlib.Path(path)
+        absolute_txt_file = txt_file.resolve()
+        #assert absolute_txt_file.exists(), 'file not found'
+        with open(absolute_txt_file, 'r', encoding='utf-8') as f:
+            assert f.read() != '', 'the note is blank, no passwords are stored here'
+            passwords_list = f.read().splitlines()
+            f.close()
+
+            return passwords_list
+    
+    except FileNotFoundError:
+        print(f'file {path} not found')
+        break
+         
             
     pass
 
+def keep_passwords_safe(path : str) -> None:
+    '''
+    overwrite the file with a blanck note to keep safe your passrords
 
-def note_is_empty(path : str) -> bool:
+    Parameters:
+    path : pathlib.Path - the absolute-/path of the note to overwrite
+    '''
+    try:            
+        txt_file = pathlib.Path(path)
+        absolute_txt_file = txt_file.resolve()
+        assert path.exists(), 'file not found'
+        with open(absolute_txt_file, 'w', encoding='utf-8') as f:
+            f.write('')
+            f.close()
+        with open(absolute_txt_file, 'r', encoding='utf-8') as w: 
+            if w.read() == '':
+                print('the note is now blank, your passwords are safe')
+        return None
+    except SyntaxError as e:
+        print(f'{e} in keep_passwords_safe function')
+
+
+def note_is_empty(path : pathlib.Path) -> bool:
     '''
     check if the note is blank
 
     Parameters:
-    path : str - the absolute-/path of the note to check
+    path : pathlib.Path - the absolute-/path of the note to check
     '''
+
     with open(path, 'r', encoding='utf-8') as f: # TODO test the encoding
         if f.read() == '':
             print('the note is blank, no passwords are stored here')
