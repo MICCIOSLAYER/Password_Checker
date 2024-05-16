cf38faa52b51d730b7be71d740e510e6d8a44024
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-05-16 18:29:25 +0200

    reorganization of the scripts to define the modules and tests subpackages

--- build/lib/Check_these_passwords/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""__init__.py file for command line application."""
-
-__author__ = 'Renato Eliasy'
-__email__ = 'renato.eliasy@studio.unibo.it'
--- build/lib/Check_these_passwords/changes_todo.py
+++
@@ -1,2 +0,0 @@
-# here a list of possible change to do in the code, to let the code in a more ordered way:
-
