c4ee20b135575970c68f463289ab72f332ca1541
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-05-17 12:12:41 +0200

    DRY: fixed the sha_protocol

--- Password_Checker/__main__.py
+++ Password_Checker/__main__.py
@@ -16,10 +16,19 @@ import getpass
 import logging
 import sys
 
-logging.basicConfig(level=logging.DEBUG, filename='log.log',filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
+#LOGGING CONFIGURATION
+logging.basicConfig(
+    level=logging.DEBUG, 
+    filename='main_log.log',
+    filemode='w', 
+    format='%(asctime)s - %(levelname)s - %(message)s', 
+    encoding='utf-8')
 
-# ESECUZIONE DEL PROGRAMMA 2.0
-def core_execution(passwords_list : list, sha_protocol : bool)-> None: # a function to slim the code in main()
+# PROGRAM EXECUTION 2.0
+def core_execution(
+        passwords_list : list, 
+        sha_protocol : bool
+        )-> None: # a function to slim the code in main()
     '''
     Parameters:
     - passwords_list : list - the list of passwords to check
@@ -29,6 +38,7 @@ def core_execution(passwords_list : list, sha_protocol : bool)-> None: # a funct
     if len(passwords_list) <= 0:
         print('the list is empty, no passwords to check')
     else:
+        sha_protocol = False # when implemented use remove this line
         for password in passwords_list:
             count = API_functions.conta_trapelate(API_functions.pwned_API_check(password, sha256=sha_protocol))
             if count:
@@ -43,31 +53,54 @@ def core_execution(passwords_list : list, sha_protocol : bool)-> None: # a funct
 def main():
     logging.basicConfig(filename='mainlog.log',
                         level=logging.INFO,
-                        format='%(asctime)s %(levelname)-8s %(module)s - %(funcName)s: %(message)s',
+                        format='%(asctime)s %(levelname)-8s %(name)s - %(funcName)s: %(message)s',
                         datefmt='%Y-%m-%D %H:%M:%S')
  
     parser = argparse.ArgumentParser(description='check the reliability of your passwords')
-    sha256_protocol = parser.add_argument('--sha256', help='unable to use the sha256 protocol, since not implemtented yet in the code functionalities', action='store_true') # FIXME use a False default value
+
+    sha256_protocol = parser.add_argument(
+        '--sha256', 
+        help='unable to use the sha256 protocol, since not implemtented yet in the code functionalities', 
+        action='store_true') 
+
 
     reading_mode = parser.add_mutually_exclusive_group(required=False) 
-    parser_pwcl = reading_mode.add_argument('-fh', '--from_here', help='input the passwords here, following the command to get them', action='store_true') 
-    parser_example = reading_mode.add_argument('-ex', '--example', help='use an example to show the program', action='store_true', default=False)
-    parser_file = reading_mode.add_argument('-fl', '--from_file', help='input the path to password\'s file, otherwise remember to fill the default file in your Desktop', default=manage_file.default_file_path(), type=Path) 
-    verification_parser = reading_mode.add_argument('-v', '--verify', help='analyze and testing', type=Path, default=None) # TODO remove once test on it are finished
+    parser_pwcl = reading_mode.add_argument(
+        '-fh', '--from_here', 
+        help='input the passwords here, following the command to get them', 
+        action='store_true') 
+    
+    parser_example = reading_mode.add_argument(
+        '-ex', '--example', 
+        help='use an example to show the program', 
+        action='store_true', 
+        default=False)
+    
+    parser_file = reading_mode.add_argument(
+        '-fl', '--from_file', 
+        help='input the path to password\'s file, otherwise remember to fill the default file in your Desktop', 
+        default=manage_file.default_file_path(), 
+        type=Path) 
+    
+    verification_parser = reading_mode.add_argument(# TODO remove once test on it are finished
+        '-v', '--verify', 
+        help='analyze and testing', 
+        type=Path, 
+        default=None) 
 
     args =  parser.parse_args()
-    logging.info(f'\nargs: {args} \n')
-    logging.info(f'args.verify: {args.verify} \nargs.example: {args.example} \nargs.from_file: {args.from_file} \nargs.from_here: {args.from_here} \nargs.sha256: {args.sha256}\n')
+    logging.debug(f'\nargs: {args} \n')
+    logging.debug(f'\nargs.verify: {args.verify} \nargs.example: {args.example}  \nargs.from_here: {args.from_here} \nargs.sha256: {args.sha256}\nargs.from_file: {args.from_file}')
+    
     sha_protocol = args.sha256
     if sha_protocol: 
-        logging.warning('this option is not available yet, then the passwords will be checked with sha1 protocol')
-        sha_protocol = False  
+        logging.warning('not available yet, then checked using sha1 protocol')
+        
 
 
     if args.example:
-        example_path = manage_file.example_file() #HACK after testing replace the path with a simple list of strings
-        core_execution(manage_file.txt_to_list(example_path), sha_protocol)
-        logging.info('since this is an example, the passwords are casual and to be cancelled by the real file once the code is ok')
+        core_execution(manage_file.txt_to_list(manage_file.example_file()), sha_protocol)
+        logging.info('this is a fixed example no need to keep safe the passwords')
         sys.exit() 
 
     elif args.from_here: 
@@ -81,10 +114,10 @@ def main():
         print(f'all\'interno di args.verify vi è assegnato il valore : {args.verify} ')
         sys.exit() #FIXED?
 
-    #it has to be at the end to avoid the execution of the default file 
-    elif args.from_file:
-        the_file = pathlib.Path(args.from_file)
-        core_execution(manage_file.txt_to_list(the_file), sha_protocol)
+    
+    elif args.from_file:  #  It has to be at the end to avoid the execution of the default file 
+        the_file = Path(args.from_file)
+        core_execution(manage_file.txt_to_list(the_file), sha_protocol) # when implemented use sha_protocol
         manage_file.keep_passwords_safe(the_file)
         sys.exit() 
 
