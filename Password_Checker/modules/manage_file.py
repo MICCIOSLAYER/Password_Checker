6ae1927687c7d209cd22622e49fa5c602189f966
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-05-20 16:36:19 +0200

    increase logging informations, reduce comments on code

--- Password_Checker/modules/manage_file.py
+++ Password_Checker/modules/manage_file.py
@@ -5,40 +5,36 @@
 __author__ = 'Renato Eliasy'
 __email__ = 'renato.eliasy@studio.unibo.it'
 
-import pathlib
+
 from pathlib import Path
 import os
 import logging
 
-
- 
-
-def txt_to_list(txt_file : pathlib.Path ) -> list[str]: # FIXED
+def txt_to_list(txt_file : Path ) -> list[str]: 
     '''
-    get the list of passwords from the note
+    Put the txt file content in a list of Strings
     
     Parameters:
     path : str - the absolute-path of the note from which to get the passwords
     '''
-     
     try:
         if str(txt_file)[-4:]  != '.txt': 
             logging.critical(f'{txt_file} is not a txt file') 
             raise TypeError('this is not a txt file')
-        # to avoid unreadable character use a default encoding depending on your pc? or better to use utf-8
+       
         with open(txt_file, 'r', encoding='utf-8', errors='strict') as f: 
                 passwords_list = f.read().split()
                 f.close()
                 if len(passwords_list) == 0:
                     logging.warning('no passwords are stored here')
+
     except FileNotFoundError: 
         logging.exception(f'file {txt_file} not found') 
         return 'file not found'
-    #for completeness eve if the test doesn't seems to care
-    except ValueError as e: # if a decoding error occurs open(error=strict by default) raise a ValueError
+    except ValueError as e: #for completeness eve if the test doesn't seems to care
         logging.exception(f'{e} use a UTF-8 encoded file to store your passwords')
         return 'Unreadable file, assure to use a UTF-8 encoded file to store your passwords'
-    except Exception as e: # HOWTO test this? 
+    except Exception as e: 
         logging.exception(f'{e} during the reading of the file {txt_file}')   
         return 'Unexpected exception read the log file for more details'
     
@@ -47,42 +43,44 @@ def txt_to_list(txt_file : pathlib.Path ) -> list[str]: # FIXED
     
      
             
-def default_file_path() -> pathlib.Path: # FIXED 
-    '''                                                 con path  = txt_file_default (oggetto pathlib.Path)
+def default_file_path() -> Path:
+    '''                                                 
     creation/ read of a file to read passwords by a default path in Desktop
+    if nothing inside put some content as a default content
     '''
     
-    desktop_path = pathlib.Path(os.path.expanduser("~/Desktop"))
-    txt_file_default = desktop_path / 'default_list.txt' # TODO replace it with a config file object
-    content = 'D_default_path4 these@password isins1de thi5Pc'  # TODO replace it with a config file object
-    logging.info(f'this is a default_path list of passwords: {txt_file_default}')
+    desktop_path = Path(os.path.expanduser("~/Desktop"))
+    txt_file_default = desktop_path / "default_list.txt"
+    content = 'D_default_path4 these@password isins1de thi5Pc'
+    logging.debug(f'this is a default_path list of passwords: {txt_file_default}')
 
     
-    if os.path.exists(txt_file_default):  # if exists write it if blank
+    if os.path.exists(txt_file_default):  
         with open(txt_file_default, 'r+', encoding='utf-8') as b: 
             blanked= b.read() ==  ''
-            if blanked:  # to avoid the overwrite of the file if not empty
+            if blanked:  #  overwrite of the file if empty
                 b.write(content) 
             b.close()
         if blanked: # necessary outside to logging correctly
             logging.warning(f'the file {txt_file_default} is blank, then it will be overwritten')
 
-    else:   # if not exists create it
+    else:   # if not found in designated path create it
         logging.warning(f'since {txt_file_default} is not found, it will be created')
         txt_file_default.parent.mkdir(exist_ok=True, parents=True)
         with open(txt_file_default, 'w', encoding='utf-8') as f:  
-            f.write(content)  # TODO replace it with a config file object
+            f.write(content)  
             f.close()
 
     return (txt_file_default)
 
     
 
-def keep_passwords_safe(txt_file : pathlib.Path) -> None:
+def keep_passwords_safe(txt_file : Path) -> None:
     '''
     overwrite the file with a blanck note to keep safe your passrords
 
     Parameters:
+
     txt_file : Path - the absolute-path of the note containing your passwords to overwrite
     '''
     
@@ -102,12 +100,12 @@ def keep_passwords_safe(txt_file : pathlib.Path) -> None:
         logging.info('the note is now blank')  
         return 'Passwords aren\'t leaked'
 
-def example_file() -> pathlib.Path:
+def example_file() -> Path:
     '''
-    creation/ read of a file to read passwords by a default path in Desktop
+    creation of a file as an example 
     '''
-    example_path = pathlib.Path.home() / 'Check_these_passwords' / 'example_list.txt'  # TODO replace it with a config file object
-    content = 'today@is @b34UtIfull \nTh4n Y3sterd@y'   # TODO replace it with a config file object
+    example_path = Path.home() / "Check_these_passwords" / "example_list.txt"
+    content = 'today@is @b34UtIfull\nTh4n Y3sterd@y'
     example_path.parent.mkdir(exist_ok=True, parents=True)
     with open(example_path, 'w', encoding='utf-8') as f:
         f.write(content)
