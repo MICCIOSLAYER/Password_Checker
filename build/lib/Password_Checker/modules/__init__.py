7c290bc5b9147bed0ff1cb4945c75641888aea53
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-05-22 12:50:29 +0200

    removed build to not create conflict while downloads

--- build/lib/Password_Checker/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-__init__.py file for command line application
-to set these files as a module"""
-
-__author__ = 'Renato Eliasy'
-__email__ = 'renato.eliasy@studio.unibo.it'
