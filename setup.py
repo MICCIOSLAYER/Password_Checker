801f253beecf0fd3df4cb34dc030ab35d6ba8cd1
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-05-22 12:49:39 +0200

    fix minor bug to enable --editable command

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
-setup( 
-    name= 'Password_Checker',
-    version= '0.1.0',
-    requires= ['requests', 'hashlib', 'sys', 'os', 're', 'argparse', 'response', 'pathlib', 'getpass', 'logging', 'configparser',],
-    packages= find_packages(),
-    entry_points= {
+setup(
+    name='Password_Checker',
+    version='0.1.0',
+    requires=['requests', 'hashlib', 'sys', 'os', 're', 'argparse',
+              'response', 'pathlib', 'getpass', 'logging', 'configparser'],
+    packages=find_packages(),
+    entry_points={
         'console_scripts': [
-            'Check-these-passwords = Password_Checker.__main__:main' 
+            'Check-these-passwords = Password_Checker.__main__:main'
         ]
     },
     url='https://github.com/MICCIOSLAYER/Password_Checker.git',
