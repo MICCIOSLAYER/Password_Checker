fe7db4270be561ffab66a420511521d763aed8d2
MICCIOSLAYER <101573561+MICCIOSLAYER@users.noreply.github.com>
2024-06-22 16:46:23 +0200

    added author and email in the script

--- Password_Checker/modules/main_execution.py
+++ Password_Checker/modules/main_execution.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""__init__.py file for command line application."""
+
+__author__ = 'Renato Eliasy'
+__email__ = 'renato.eliasy@studio.unibo.it'
+
+
 from modules import API_functions
 import sys
 
